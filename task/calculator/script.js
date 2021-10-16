f5f854f859a25784bec467ae4242cd1820a30721
Anand <anand.kva00@gmail.com>
2021-10-16 16:16:46 +0530

    calculator task commit

--- task/calculator/script.js
+++ task/calculator/script.js
@@ -1,23 +1,44 @@
 const display = (val) => {
-    document.getElementById("result").value += val
-}
+    let screen = document.getElementById("result")
+    screen.value += val
+};
 const solve = () => {
+    let screen = document.getElementById("result")
     try {
-        let x = document.getElementById("result").value
+        let x = screen.value
         if (x == "") {
-            document.getElementById("result").value = "0"
-        }else {
+            screen.value = "0"
+        } else {
             let y = eval(x)
-            document.getElementById("result").value = y
+            screen.value = y
         }
     }
     catch (err) {
-        document.getElementById("result").value = err
+        screen.value = err
     }
-}
+};
 const clr = () => {
     document.getElementById("result").value = ""
-}
+};
 const del = () => {
-    document.getElementById("result").value = document.getElementById("result").value.slice(0, -1)
+    document.getElementById("result").value = screen.value.slice(0, -1)
+};
+const validate = (evt) => {
+    var theEvent = evt || window.event;
+    // Handle paste
+    if (theEvent.type === 'keyV') {
+       key = event.clipboardData.getData('text/plain');
+       console.log("worked")
+       
+    } else {
+        // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9 \+ \- /* ]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        alert("Only Numbers")
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
 }
